419abfad7ca49ddd5d8496d344f4fe5b0d8c7b4a
tonyschneider <tonysch05@gmail.com>
2022-02-03 23:35:46 +0200

    deploy screen session

--- cicd-jenkinsfile.groovy
+++ cicd-jenkinsfile.groovy
@@ -4,11 +4,26 @@ node {
             echo 'Cleaning work space'
         }
         stage('Pull updated code'){
-            dir('/home/tony/telebots'){
+            dir('/home/jenkins/telebots'){
                 sh """
-                su tony
+                echo 'pulling code'
                 git pull
                 """
+            }
+        }
+        stage('Deploy bot services'){
+            dir('/home/jenkins/telebots'){
+                 withCredentials([string(credentialsId: 'english_token_bot', variable: 'TOKEN'),
+                                  usernamePassword(credentialsId: 'mysql_credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
+                    sh """
+                    echo 'Sets necessary environment variables'
+                    export TONY_ENGLISH_BOT_TOKEN='${SECRET}' && export MYSQL_USER='${username}' && export MYSQL_PASS='${password}'
+                    echo 'Killing all screen sessions'
+                    killall screen
+                    echo 'attaching new screen session'
+                    screen -d -m python3 telebots/tony_english_bot.py
+                    """
+                }
             }
         }
     }
