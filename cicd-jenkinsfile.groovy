7d34ab4c9307a75bdd538577b4032128dff19133
tonyschneider <tonysch05@gmail.com>
2022-02-04 20:35:32 +0200

    improvements

--- cicd-jenkinsfile.groovy
+++ cicd-jenkinsfile.groovy
@@ -15,14 +15,13 @@ node {
             withCredentials([string(credentialsId: 'english_token_bot', variable: 'TOKEN'),
                                       usernamePassword(credentialsId: 'mysql_credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
                 sh """
-                    su tony
                     cd /home/tony
                     echo 'Sets necessary environment variables'
-                    export TONY_ENGLISH_BOT_TOKEN='${TOKEN}' && export MYSQL_USER='${username}' && export MYSQL_PASS='${password}'
+                    sudo export TONY_ENGLISH_BOT_TOKEN='${TOKEN}' && export MYSQL_USER='${username}' && export MYSQL_PASS='${password}'
                     echo 'Killing all screen sessions'
-                    screen -ls | grep '(Detached)' | awk 'sys {screen -S ${1} -X quit}'
+                    sudo screen -ls | grep '(Detached)' | awk 'sys {screen -S ${1} -X quit}'
                     echo 'attaching new screen session'
-                    screen -d -m python3 telebots/tony_english_bot.py
+                    sudo screen -d -m python3 telebots/tony_english_bot.py
                 """
             }
         }
